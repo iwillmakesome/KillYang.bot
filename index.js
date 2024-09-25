10dfa023c6027ebf1595bf8178d492c780c9e0bd
itismilob <itismilob@gmail.com>
2024-09-25 22:59:42 +0900

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const {
+  Client,
+  GatewayIntentBits,
+  SlashCommandBuilder,
+} = require("discord.js");
+const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
+const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
+
+require("dotenv").config();
+client.login(process.env.TOKEN);
+client.once("ready", () => {
+  console.log(client.user.tag + " ready!");
+  client.application.commands.create(
+    new SlashCommandBuilder().setName("test").setDescription("test")
+  );
+});
+
+client.on("messageCreate", (msg) => {
+  const content = msg.content;
+
+  if (content == ":emoji_3:" || content == "<:emoji_3:877020440093163601>") {
+    msg.reply("죽어 양범건ㅋ");
+  }
+});
