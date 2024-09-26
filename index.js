cd7bb6a1dfb83109c34ec9ad17e0ea309666d6ee
itismilob <itismilob@gmail.com>
2024-09-26 21:02:44 +0900

    token test

--- index.js
+++ index.js
@@ -6,19 +6,28 @@ const {
 const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
 const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
 
-require("dotenv").config();
-client.login(process.env.token);
-client.once("ready", () => {
-  console.log(client.user.tag + " ready!");
-  client.application.commands.create(
-    new SlashCommandBuilder().setName("test").setDescription("test")
-  );
-});
+console.log("######################################");
+console.log("######################################");
+console.log("######################################");
+console.log("######################################");
+console.log(process.env.token);
+console.log("######################################");
+console.log("######################################");
+console.log("######################################");
+console.log("######################################");
 
-client.on("messageCreate", (msg) => {
-  const content = msg.content;
+// client.login(process.env.token);
+// client.once("ready", () => {
+//   console.log(client.user.tag + " ready!");
+//   client.application.commands.create(
+//     new SlashCommandBuilder().setName("test").setDescription("test")
+//   );
+// });
 
-  if (content == ":emoji_3:" || content == "<:emoji_3:877020440093163601>") {
-    msg.reply("죽어 양범건ㅋ");
-  }
-});
+// client.on("messageCreate", (msg) => {
+//   const content = msg.content;
+
+//   if (content == ":emoji_3:" || content == "<:emoji_3:877020440093163601>") {
+//     msg.reply("죽어 양범건ㅋ");
+//   }
+// });
